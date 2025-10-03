56a1535cdf2af2f4cc5078046562e08df630c4fc
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2025-10-03 13:24:41 +0300

    Add files via upload

---
+++ lab1_0310.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+/*
+int main() {
+	printf("Hello World!");
+    printf("Ad: Nisanur\n");
+    printf("Soyad: Ozpehlivan\n");
+    printf("Numara: 170525844\n");
+return 0;
+}
+*/
+
+/*
+int main() {
+	printf("    *\n");
+    printf("   **\n");
+    printf("  ***\n");
+    printf(" ****\n");
+    printf("*****");
+return 0;
+}
+*/
+
+
+/* YANLIÞ KOD
+
+// Laboratuvar Uygulamasý L01U03
+
+#include <stdio.h>;
+
+
+{
+
+int main()
+printf("Merhaba BLM1005")
+
+Return 0;
+ }
+ */
+
+ // DüZENLENMÝÞ KOD
+
+ #include <stdio.h>
+
+int main() {
+	printf("Merhaba BLM1005");
+
+return 0;
+}
