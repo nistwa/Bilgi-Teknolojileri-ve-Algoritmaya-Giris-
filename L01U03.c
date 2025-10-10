d9eeb0fd086fab5be62f6e5065f94cfae25d6ca2
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2025-10-10 11:13:16 +0300

    Add files via upload

---
+++ L01U03.c
@@ -0,0 +1,26 @@
+//Laboratuvar Uygulamas� L01U03
+/*
+ YANLIS KOD
+
+ #include <stdio.h>;
+
+
+ {
+
+ int main()
+ printf("Merhaba BLM1005")
+
+ Return 0;
+ }
+*/
+
+ // DUZENLENMIS KOD
+
+  #include <stdio.h>
+
+ int main() {
+ 	printf("Merhaba BLM1005");
+
+ return 0;
+ }
+
