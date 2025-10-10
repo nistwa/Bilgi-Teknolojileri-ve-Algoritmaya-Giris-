0aece302efc388050ced3817b53649e35cb2f8fe
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2025-10-10 10:57:28 +0300

    Add files via upload

---
+++ L01U01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello World!");
+    printf("Ad: Nisanur\n");
+    printf("Soyad: Ozpehlivan\n");
+    printf("Numara: 170525844\n");
+return 0;
+}
